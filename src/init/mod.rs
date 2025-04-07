79c4930d9d0d1f2e8bc81e561205b2009deb6927
hackia <hackia@eytukan.com>
2025-04-07 16:30:18 +0200

    start feat

---
+++ src/init/mod.rs
@@ -0,0 +1,1 @@
+pub fn init(name: &str) {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use crate::init::init;
+use crate::run::run;
+use clap::{Parser, Subcommand};
+use crate::build::build;
+use crate::seal::seal;
+use crate::share::share;
+
+pub mod init;
+pub mod run;
+pub mod build;
+pub mod share;
+pub mod seal;
+#[derive(Parser)]
+#[command(name = "lys")]
+#[command(version, about = "Planpkg: ethical, portable, living packages")]
+struct Lys {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Init { name: String },
+    Build,
+    Run,
+    Seal,
+    Share,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lys = Lys::parse();
+
+    match lys.command {
+        Command::Init { name } => init(name.as_str()),
+        Command::Build => build(),
+        Command::Run => run(),
+        Command::Seal => seal(),
+        Command::Share => share(),
+    }
 }
