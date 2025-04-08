84d86efab62d3895c79522a439a7547dd61ddf87
hackia <hackia@eytukan.com>
2025-04-08 03:06:41 +0200

    add push & pull

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use anyhow::Error;
-use build::build_img;
+use crate::build::IMAGES_DIR;
+use anyhow::{Error, anyhow};
+use build::{build_img, run_command};
 use clap::{Parser, Subcommand};
 use init::init;
 use run::run;
-
 pub mod build;
 pub mod init;
 pub mod run;
 pub mod seal;
 pub mod share;
+
 #[derive(Parser)]
 #[command(name = "lys")]
 #[command(version, about = "Ethical, portable, living packages")]
@@ -28,8 +29,16 @@ enum Command {
     Run {
         name: String,
     },
-    Pull {},
-    Push {},
+    Pull {
+        os: String,
+        user: String,
+        hostname: String,
+    },
+    Push {
+        os: String,
+        user: String,
+        hostname: String,
+    },
     Clone {},
     Prune {},
     Ps {},
@@ -45,11 +54,45 @@ fn main() -> Result<(), Error> {
             init(os.as_str(), p.as_str(), e.eq(&"yes"), tmp.eq(&"yes"))
         }
         Command::Run { name } => run(name.as_str()),
-        Command::Pull {} => todo!(),
+        Command::Pull { os, user, hostname } => pull(os.as_str(), user.as_str(), hostname.as_str()),
         Command::Clone {} => todo!(),
         Command::Prune {} => todo!(),
         Command::Ps {} => todo!(),
         Command::List {} => todo!(),
-        Command::Push {} => todo!(),
+        Command::Push { os, user, hostname } => push(os.as_str(), user.as_str(), hostname.as_str()),
     }
+}
+
+fn pull(os: &str, username: &str, hostname: &str) -> Result<(), Error> {
+    if run_command(
+        "rsync",
+        &[
+            "-a",
+            "-v",
+            "-z",
+            "--delete",
+            format!("{username}@{hostname}:{IMAGES_DIR}/{os}.img").as_str(),
+            format!("{IMAGES_DIR}/{os}.img").as_str(),
+        ],
+    )? {
+        return Ok(());
+    }
+    Err(anyhow!("failed to pull image"))
+}
+
+fn push(os: &str, username: &str, hostname: &str) -> Result<(), Error> {
+    if run_command(
+        "rsync",
+        &[
+            "-a",
+            "-v",
+            "-z",
+            "--delete",
+            format!("{IMAGES_DIR}/{os}.img").as_str(),
+            format!("{username}@{hostname}:{IMAGES_DIR}/{os}.img").as_str(),
+        ],
+    )? {
+        return Ok(());
+    }
+    Err(anyhow!("failed to pull image"))
 }
